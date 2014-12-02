e402ccc882258bb0700a3f42a34567c100919a87
Monster <naocanmonster@gmail.com>
2014-12-02 21:13:34 +0800

    fixed param

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ def main():
     from optparse import OptionParser
     msgUsage = "%prog"
     opt = OptionParser(msgUsage)
-    opt.add_option("-v", "--victim",  action = "store", dest = "gateway", help = "victim IP address")
+    opt.add_option("-v", "--victim",  action = "store", dest = "victim", help = "victim IP address")
     opt.add_option("-t", "--target",  action = "store", dest = "target", help = "target IP you want to poisoning")
-    opt.add_option("-g", "--gateway", action = "store", dest = "victim", help = "new gateway for the poisoned destination")
+    opt.add_option("-g", "--gateway", action = "store", dest = "gateway", help = "new gateway for the poisoned destination")
     opt.add_option("-s", "--source", action = "store", dest = "source", help = "source IP address of the ICMP message")
     args = opt.parse_args(argv[1:])
     if not args[0].victim or not args[0].target or not args[0].source or not args[0].gateway:
